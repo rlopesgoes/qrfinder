3fd7f68ded7ca32a27d3bcdac842c16c6810ce12
Renato Jose da Silva <renatojsilvas@gmail.com>
2025-09-20 13:20:16 -0300

    Adjust swagger

--- src/WebApi/Endpoints/Videos.cs
+++ src/WebApi/Endpoints/Videos.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Application.Queries;
 using MediatR;
 
@@ -5,15 +6,36 @@ namespace WebApi.Endpoints;
 
 public static class Videos
 {
+    private const string QrCodeFinder = "QR Code finder";
+
     public static void MapVideosEndpoints(this WebApplication app)
     {
-        app.MapGet("/test", async (IMediator mediator, string? message) =>
+        app.MapPost("/video/upload", async (IMediator mediator, string? message) =>
+            {
+                return Results.Ok(true);
+            })
+            .WithName("Upload video for qr code finding")
+            .WithTags(QrCodeFinder)
+            .WithOpenApi();
+        
+        app.MapGet("/video/{id:guid}/status", async (IMediator mediator, Guid id, string? message) =>
             {
                 var query = new TestQuery(message ?? "Hello from MediatR!");
                 var result = await mediator.Send(query);
                 return Results.Ok(result);
             })
-            .WithName("TestMediatR")
+            .WithName("Get status of video processing")
+            .WithTags(QrCodeFinder)
+            .WithOpenApi();
+        
+        app.MapGet("/video/{id:guid}/results", async (IMediator mediator, Guid id, string? message) =>
+            {
+                var query = new TestQuery(message ?? "Hello from MediatR!");
+                var result = await mediator.Send(query);
+                return Results.Ok(result);
+            })
+            .WithName("Get results of qr codes finding")
+            .WithTags(QrCodeFinder)
             .WithOpenApi();
     }
 }
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -2,6 +2,7 @@ using DotNetEnv;
 using Application;
 using Application.Queries;
 using MediatR;
+using Microsoft.OpenApi.Models;
 using WebApi;
 using WebApi.Endpoints;
 
@@ -9,25 +10,18 @@ Env.TraversePath().Load();
         
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddAuthorization();
-        
-// Add MediatR
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(AssemblyReference).Assembly));
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc(name: "v1", new OpenApiInfo { Title = "QrFinder API", Version = "v1" });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapVideosEndpoints();
 

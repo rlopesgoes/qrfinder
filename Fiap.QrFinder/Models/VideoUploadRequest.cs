190dec28d7f685fa682fabb402018fb1ea693f93
Renato Jose da Silva <renatojsilvas@gmail.com>
2025-09-20 11:21:02 -0300

    Create Clean Arch structure

--- Fiap.QrFinder/Models/VideoUploadRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace Fiap.QrFinder.Models;
-
-public class VideoUploadRequest
-{
-    public IFormFile VideoFile { get; set; } = null!;
-    public int? FrameInterval { get; set; }
-}
--- Fiap.QrFinder/Services/IVideoProcessingService.cs
+++
@@ -1,9 +0,0 @@
-using Fiap.QrFinder.Models;
-
-namespace Fiap.QrFinder.Services;
-
-public interface IVideoProcessingService
-{
-    Task<string> SaveUploadedFileAsync(IFormFile file);
-    Task<VideoProcessingResult> ProcessVideoAsync(string filePath, int frameInterval = 1);
-}
